cc88bfa65c5dde71f7aa74015f793d03feacdcf8
Gaganapa <Gaganapa@DIN76000002.corp.capgemini.com>
2019-07-04 14:41:15 +0530

    Login page commits

---
+++ MyFirstDemo/src/main/java/org/cap/login/Login.java
@@ -0,0 +1,12 @@
+package org.cap.login;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Login Page Branch");
+
+	}
+
+}
