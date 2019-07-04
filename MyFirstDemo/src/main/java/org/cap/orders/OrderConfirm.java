6d38050d5ae4d16c2167f7cd2cfbf559269568ae
Gaganapa <Gaganapa@DIN76000002.corp.capgemini.com>
2019-07-04 14:45:08 +0530

    Order page info was completed

---
+++ MyFirstDemo/src/main/java/org/cap/orders/OrderConfirm.java
@@ -0,0 +1,10 @@
+package org.cap.orders;
+
+public class OrderConfirm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Order Confirmed");
+	}
+
+}
