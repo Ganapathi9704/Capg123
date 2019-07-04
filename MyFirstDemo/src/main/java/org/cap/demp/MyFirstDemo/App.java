1e84aacd7e6d14060dd1016ebd890275e636bed1
Gaganapa <Gaganapa@DIN76000002.corp.capgemini.com>
2019-07-04 11:33:37 +0530

    My First Git Project

---
+++ MyFirstDemo/src/main/java/org/cap/demp/MyFirstDemo/App.java
@@ -0,0 +1,13 @@
+package org.cap.demp.MyFirstDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
